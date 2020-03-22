125e64d9c75cd38c24b7202c54561ae3500fb5a2
Muhammed Roshan <muhammedroshan222@gmail.com>
2020-03-22 23:24:54 +0530

    Added ploting test and train graph

--- Assignment10/traing.py
+++ Assignment10/traing.py
@@ -15,6 +15,8 @@ def train(model, device, train_loader, optimizer, epoch):
   correct = 0
   processed = 0
   train_loss = 0
+  #train_acc = []
+  #train_losses = []
   criterion = nn.CrossEntropyLoss()
   for batch_idx, (data, target) in enumerate(pbar):
     # get samples
@@ -40,12 +42,16 @@ def train(model, device, train_loader, optimizer, epoch):
     correct += pred.eq(target.view_as(pred)).sum().item()
     processed += len(data)
     pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
-    #train_acc.append(100*correct/processed)
+  train_losses.append(loss) # train_loss
+  train_acc.append(100*correct/processed)
+  #return train_losses, train_acc
 
 def test(model, device, test_loader):
     model.eval()
     test_loss = 0
     correct = 0
+    #test_acc = []
+    #test_losses = []
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
@@ -61,19 +67,38 @@ def test(model, device, test_loader):
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
     
-    #test_acc.append(100. * correct / len(test_loader.dataset))
+    test_acc.append(100. * correct / len(test_loader.dataset))
     return test_loss
 
 def Training(epochs,model,device, trainloader, testloader):
   Testloss = 0
+  # trainLoss = []
+  # trainAccu = []
+  # testLoss = []
+  # testAccu = []
   optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.95)
-  scheduler = ReduceLROnPlateau(optimizer, 'min')#StepLR(optimizer, step_size=6, gamma=0.1)
+  scheduler = ReduceLROnPlateau(optimizer, 'min') #StepLR(optimizer, step_size=6, gamma=0.1)
   for epoch in range(epochs):
       print("EPOCH:", epoch)
       train(model, device, trainloader, optimizer, epoch)
       Testloss = test(model, device, testloader)
       scheduler.step(Testloss)
 
+def plotPerformanceGraph():
+  import matplotlib.pyplot as plt
+  fig, (axs1,axs2) = plt.subplots(2, 1,figsize=(15,10))
+
+  axs1.plot(train_losses, label = " Train Loss")
+  axs1.plot(test_losses, label = " Test Loss")
+  axs1.set_title(" Loss")
+
+  axs2.plot(train_acc, label = " Train Accuracy")
+  axs2.plot(test_acc, label = " Test Accuracy")
+
+  axs2.set_title(" Accuracy")
+  axs1.legend()
+  axs2.legend()
+  plt.show()
 
 def ClassTestAccuracy(testloader,device,model, classes):
   class_correct = list(0. for i in range(10))
@@ -95,3 +120,6 @@ def ClassTestAccuracy(testloader,device,model, classes):
   for i in range(10):
       print('Accuracy of %5s : %2d %%' % (
           classes[i], 100 * class_correct[i] / class_total[i]))
+
+
+
