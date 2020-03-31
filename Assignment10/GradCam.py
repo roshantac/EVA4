06a20713d78dc49ab4ce4ac0ceaadc17bc111cd4
Muhammed Roshan <muhammedroshan222@gmail.com>
2020-03-31 08:59:19 +0530

    Resolved issues in the gradcam

--- Assignment10/GradCam.py
+++ Assignment10/GradCam.py
@@ -68,10 +68,10 @@ class GradCAM:
 
 
 
-def show_map(img):
-    target_model = ResNet18()
+def show_map(img,model):
+    target_model = model
     gradcam = GradCAM.from_config(model_type='resnet', arch=target_model, layer_name='layer4')
-    img = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])(img)[None]
+    img = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])(img)[None] #
     mask, logit = gradcam(img)
     heatmap, cam_result = visualize_cam(mask, img)
     return heatmap, cam_result
