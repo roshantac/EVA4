7519cbc93115578a014965a48875db0cbbac38e4
Muhammed Roshan <muhammedroshan222@gmail.com>
2020-04-12 01:15:42 +0530

    Updated 11 th assignment  with one cycle learning policy

---
+++ Assignment 11/dataset.py
@@ -0,0 +1,65 @@
+
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+#import Albumentation
+
+import numpy as np
+from albumentations import Compose, RandomCrop,PadIfNeeded, Normalize, HorizontalFlip,HueSaturationValue,Cutout,ShiftScaleRotate
+from albumentations.pytorch import ToTensor
+import cv2 
+
+
+class album_Compose:
+  def __init__(self):
+    self.alb_transform =Compose([
+      PadIfNeeded(min_height=32, min_width=32, border_mode=cv2.BORDER_CONSTANT, value=4, always_apply=False, p=1.0),
+      RandomCrop(32, 32, always_apply=False, p=.50),HorizontalFlip(), 
+      #HueSaturationValue(hue_shift_limit=4, sat_shift_limit=13, val_shift_limit=9),
+      Cutout(num_holes=1, max_h_size=8, max_w_size=8),
+      #ShiftScaleRotate(shift_limit=0.05, scale_limit=0.2,rotate_limit=13, p=0.8),
+      
+      Normalize(
+        mean = [0.4914, 0.4822, 0.4465],
+        std = [0.2023, 0.1994, 0.2010],
+        ),
+      ToTensor(),
+      ],p=.5)
+  def __call__(self,img):
+    img = np.array(img)
+    img = self.alb_transform(image=img)['image']
+    return img
+
+
+def getData():
+  k= album_Compose()
+  transform_test = transforms.Compose(
+      [transforms.ToTensor(),
+      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+  transform_train = transforms.Compose([
+      transforms.RandomCrop(32, padding=4),
+      transforms.RandomHorizontalFlip(), transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
+      transforms.RandomRotation((-10.0, 10.0)), transforms.ToTensor(),
+      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+  ])
+  trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                          download=True, transform=k)
+  trainloader = torch.utils.data.DataLoader(trainset, batch_size= 500,
+                                            shuffle=True, num_workers=2)
+
+  testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                        download=True, transform=transform_test)
+  testloader = torch.utils.data.DataLoader(testset, batch_size=100,
+                                          shuffle=False, num_workers=2)
+  testset2 = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                        download=True, transform=transforms.Compose([transforms.ToTensor()]))
+  testloader2 = torch.utils.data.DataLoader(testset2, batch_size=1,
+                                          shuffle=False, num_workers=2)
+
+  classes = ('plane', 'car', 'bird', 'cat',
+            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+  return trainloader, testloader,testloader2, classes
+
+
+
---
+++ Assignment 11/training.py
@@ -0,0 +1,174 @@
+from tqdm import tqdm
+from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.nn as nn
+import torch
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
+
+def train(model, device, train_loader, optimizer, epoch):
+  model.train()
+  pbar = tqdm(train_loader)
+  correct = 0
+  processed = 0
+  train_loss = 0
+  #train_acc = []
+  #train_losses = []
+  criterion = nn.CrossEntropyLoss()
+  for batch_idx, (data, target) in enumerate(pbar):
+    # get samples
+    data, target = data.to(device), target.to(device)
+
+    # Init
+    optimizer.zero_grad()
+    # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch accumulates the gradients on subsequent backward passes. 
+    # Because of this, when you start your training loop, ideally you should zero out the gradients so that you do the parameter update correctly.
+    # Predict
+    y_pred = model(data)
+
+    # Calculate loss
+    #loss = F.nll_loss(y_pred, target)
+    loss = criterion(y_pred, target)
+    #train_losses.append(loss)
+    train_loss +=loss.item()
+    # Backpropagation
+    loss.backward()
+    optimizer.step()
+    # Update pbar-tqdm
+    pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+    correct += pred.eq(target.view_as(pred)).sum().item()
+    processed += len(data)
+    pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+  train_losses.append(loss) # train_loss
+  train_acc.append(100*correct/processed)
+  #return train_losses, train_acc
+
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    #test_acc = []
+    #test_losses = []
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    test_losses.append(test_loss)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    
+    test_acc.append(100. * correct / len(test_loader.dataset))
+    return test_loss
+
+def update_lr(optimizer, lr):
+    for g in optimizer.param_groups:
+        g['lr'] = lr
+
+
+def OneCyclePolicy(LRmax, step, iterations):
+    LRmin = LRmax/10;
+    LRt = LRmin
+    LRvalues =[]
+    for x in range(iterations):
+        if (x<=step):
+          LRt += (LRmax - LRmin)/step
+          LRvalues.append(LRt)
+        else:
+          LRt -= (LRmax - LRmin)/(iterations-step)
+          LRvalues.append(LRt)
+    return LRvalues
+
+
+
+def Training(epochs,model,device, trainloader, testloader, LR):
+  Testloss = 0
+  LRvalues = OneCyclePolicy(LR, 5, epochs)
+  optimizer = optim.SGD(model.parameters(), lr=LR, momentum=0.95)
+  #scheduler = ReduceLROnPlateau(optimizer, 'min') #StepLR(optimizer, step_size=6, gamma=0.1)
+  for epoch in range(epochs):
+      print("EPOCH:", epoch)
+      update_lr(optimizer,LRvalues[epoch])
+      train(model, device, trainloader, optimizer, epoch)
+      Testloss = test(model, device, testloader)
+      #scheduler.step(Testloss)
+
+def plotPerformanceGraph():
+  import matplotlib.pyplot as plt
+  fig, (axs1,axs2) = plt.subplots(2, 1,figsize=(15,10))
+
+  axs1.plot(train_losses, label = " Train Loss")
+  axs1.plot(test_losses, label = " Test Loss")
+  axs1.set_title(" Loss")
+
+  axs2.plot(train_acc, label = " Train Accuracy")
+  axs2.plot(test_acc, label = " Test Accuracy")
+
+  axs2.set_title(" Accuracy")
+  axs1.legend()
+  axs2.legend()
+  plt.show()
+
+def ClassTestAccuracy(testloader,device,model, classes):
+  class_correct = list(0. for i in range(10))
+  class_total = list(0. for i in range(10))
+  with torch.no_grad():
+      for data in testloader:
+          images, labels = data
+          images =images.to(device)
+          labels = labels.to(device)
+          outputs = model(images)
+          _, predicted = torch.max(outputs, 1)
+          c = (predicted == labels).squeeze()
+          for i in range(4):
+              label = labels[i]
+              class_correct[label] += c[i].item()
+              class_total[label] += 1
+
+
+  for i in range(10):
+      print('Accuracy of %5s : %2d %%' % (
+          classes[i], 100 * class_correct[i] / class_total[i]))
+def MissClassifedImage(dataSet, model,device, dispCount,classes):
+  dataiter = iter(dataSet)
+  import matplotlib.pyplot as plt
+  import numpy as np
+  from GradCam import show_map
+  import matplotlib.pyplot as plt
+
+
+  fig, axs = plt.subplots(dispCount,3,figsize=(10,60))
+  count =0
+  while True:
+      if count >= dispCount:
+        break
+      images, labels = dataiter.next()
+      imagex = images
+      images, labels = images.to(device), labels.to(device)
+      model= model.to(device)
+      output = model(images)
+      a, predicted = torch.max(output, 1) 
+      if(labels != predicted):
+        imagex = imagex.squeeze()  
+        heat_map, result = show_map(imagex)
+        heat_map = np.transpose(heat_map, (1, 2, 0))
+        result = np.transpose(result, (1, 2, 0))
+        imagex = np.transpose(imagex, (1, 2, 0))
+        axs[count,0].imshow(imagex)
+        axs[count,1].imshow(heat_map)
+        axs[count,2].imshow(result)
+        axs[count,0].set_title("Orig: "+str(classes[labels])+", Pred: "+str(classes[predicted]))
+        fig.tight_layout(pad=3.0)
+        count = count +1
+  plt.show()
+
+
